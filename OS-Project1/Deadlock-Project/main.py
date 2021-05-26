56dab3d6c137a72000cbc3b885770d0ed47f7016
Abdelrhman Alarabawy <83235049+abdelrhman-alarabawy@users.noreply.github.com>
2021-05-26 23:15:53 +0200

    Update main.py
    The Main task is to detect if there is deadlock occur or not if it does Output Message will be printed tell how many process in the deadlock
    else the output message will tell no deadlock occur

--- OS-Project1/Deadlock-Project/main.py
+++ OS-Project1/Deadlock-Project/main.py
@@ -1,9 +1,35 @@
-from operator import add
+#from operator import add
+import numpy as np
 
 def detect(process, allocation, request, work):
-	""" TODO: Implement a selected deadlock detection algorithm, 
-		detect: receives processes, allocations, requests, and work 
-		and returns true if a deadlock is detected otherwise false.   """
+	    
+   
+        length = len(process)
+        temp_list = []
+        global check
+        check = np.repeat([False], length)
+        
+        for i in process:
+            if(i < len(process)):             
+                if(work[0] >= request[i][0] and work[1] >= request[i][1] and work[2] >= request[i][2] ):
+                    work[0:3] = (work[0] + allocation[i][0]) , (work[1] + allocation[i][1]) , (work[2] + allocation[i][2])
+                    check[i] = True
+                else:
+                    temp_list.append(i)
+#print(temp_list)
+                        
+        for i in temp_list:
+#print("line 22 : i +  " , i)
+            if(work[0] >= request[i][0] and work[1] >= request[i][1] and work[2] >= request[i][2] ):
+                work[0:3] = (work[0] + allocation[i][0]) , (work[1] + allocation[i][1]) , (work[2] + allocation[i][2])
+                check[i] = True
+#print(check)       
+        if all(check) == False:
+          return False
+        else:
+            return True
+            
+       
 
 if __name__=='__main__':
 	'''
@@ -22,6 +48,17 @@ if __name__=='__main__':
 	request=[[0, 0, 0], [2, 0, 2], [0, 0, 0], [1, 0, 0], [0, 0, 2]]
 	available=[0, 0, 0]
 
-	detect(process, allocation, request, available)
+	result = detect(process, allocation, request, available)
+print("#"*50 + "\n\n")         
 
+if not (result):
+    print("the system is in the deadlock state ")
+    print("The Processes in the deadlock are : ")
+    
+    for i in process:
+        if not(check[i]):
+            print(f"Process {process[i]}")
+else:
+    print("System is not in the deadlock state")     
 
+print("\n\n" + "#"*50)      
